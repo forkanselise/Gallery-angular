e4c1d12361eaa98095ef8527760bf39450e7b415
forkanselise <thouhidur.forkan@selise.ch>
2023-05-16 17:22:28 +0600

    lila

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_Url: 'https://mygalleyservice.onrender.com/',
+  API_Url_Wedding : "https://mygalleyservice.onrender.com/wedding-photos",
+  LoginVerify : "https://mygalleyservice.onrender.com/login"
 };
