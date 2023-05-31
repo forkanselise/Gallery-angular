25802b4b62dd668f4edb620f5aae59f3c7531023
forkanselise <thouhidur.forkan@selise.ch>
2023-05-31 17:32:47 +0600

    frontend fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
   API_Url: 'https://mygalleyservice.onrender.com/',
   API_Url_Wedding : "https://mygalleyservice.onrender.com/wedding-photos",
-  LoginVerify : "https://mygalleyservice.onrender.com/login"
+  LoginVerify : "https://mygalleyservice.onrender.com/login",
+  cloudinary_api_key: "851827938257399",
+  upload_preset: "mbmywdwu",
+  cloudinaryApi: "https://api.cloudinary.com/v1_1/demo/image/upload"
 };
